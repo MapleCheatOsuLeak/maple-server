ab3f35f68fb45ac42e751ce5b1be4fbbdfb50e9b
mrflashstudio <mrflashstudio@gmail.com>
2022-11-03 10:05:34 +0300

    add image stream stages to packet types

--- Maple-Server/Packets/PacketType.cs
+++ Maple-Server/Packets/PacketType.cs
@@ -5,6 +5,7 @@ public enum PacketType : byte
     Handshake = 0xA0,
     Login = 0xB0,
     LoaderStream = 0xC0,
-    ImageStream = 0xD0,
+    ImageStream_StageOne = 0xD0,
+    ImageStream_StageTwo = 0xD1,
     Heartbeat = 0xE0
 }
